7f41ff0931a2c0a9656e78b792f1bbfdabba84a0
AliGangeh <40417826+AliGangeh@users.noreply.github.com>
2019-05-05 13:42:14 -0700

    made algorithem use sigmoid function to determine the probability of each patient having diabetes based on 2 parameters. Currently it outputs a decimal as an output, and the line seperating the data is completely random

--- preceptron.py
+++ preceptron.py
@@ -1,11 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def draw(x1, x2):
+    ln=plt.plot(x1,x2)
+
+def sigmoid(score):
+    return 1/(1+np.exp(-score))
+
+#data points to be classified
 npts=100
 np.random.seed(0)
-topreg=np.array([np.random.normal(10, 2, npts), np.random.normal(12,2,npts)]).T
-botreg=np.array([np.random.normal(5,2, npts), np.random.normal(6,2,npts)]).T
+bias=np.ones(npts)
+topreg=np.array([np.random.normal(10, 2, npts), np.random.normal(12,2,npts), bias]).T
+botreg=np.array([np.random.normal(5,2, npts), np.random.normal(6,2,npts), bias]).T
+allpts=np.vstack((topreg, botreg))
+
+w1= -0.2
+w2= -0.35
+b=3.5
+linepara=np.matrix([w1,w2,b]).T
+x1 = np.array([botreg[:, 0].min(), topreg[:, 1].max()])
+x2 = -b / w2 + x1 * (-w1 / w2)
+linearcombo=allpts*linepara
+prob=sigmoid(linearcombo)
+print(prob)
+
 _, ax = plt.subplots(figsize=(4,4))
 ax.scatter(botreg[:, 0], botreg[:, 1], color="b")
 ax.scatter(topreg[:, 0], topreg[:, 1], color="r")
+draw(x1,x2)
 plt.show()
