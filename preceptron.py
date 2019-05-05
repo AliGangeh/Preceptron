8850a64c6c2a309bceef17dbee89c8bb9bcdcb9a
AliGangeh <40417826+AliGangeh@users.noreply.github.com>
2019-05-05 12:57:00 -0700

    created dataset for preceptron

--- preceptron.py
+++ preceptron.py
@@ -1,1 +1,11 @@
-#hi
+import numpy as np
+import matplotlib.pyplot as plt
+
+npts=100
+np.random.seed(0)
+topreg=np.array([np.random.normal(10, 2, npts), np.random.normal(12,2,npts)]).T
+botreg=np.array([np.random.normal(5,2, npts), np.random.normal(6,2,npts)]).T
+_, ax = plt.subplots(figsize=(4,4))
+ax.scatter(botreg[:, 0], botreg[:, 1], color="b")
+ax.scatter(topreg[:, 0], topreg[:, 1], color="r")
+plt.show()
