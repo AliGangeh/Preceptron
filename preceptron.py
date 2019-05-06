8b06386c454b0258ba44510b903f6a42d603910b
AliGangeh <40417826+AliGangeh@users.noreply.github.com>
2019-05-06 08:59:44 -0700

    I made it, it iterates 500 times findingk the line which seperates data best by itself

--- preceptron.py
+++ preceptron.py
@@ -1,32 +1,47 @@
+#imports libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
-def draw(x1, x2):
+#plots lines wait for fraction of second then removes it
+def draw(x1,x2):
     ln=plt.plot(x1,x2)
+    plt.pause(0.0001)
+    ln[0].remove()
 
 def sigmoid(score):
-    return 1/(1+np.exp(-score))
+  return 1/(1+np.exp(-score))
 
-#data points to be classified
-npts=100
+def calculate_error(line_parameters, points , y):
+  n=points.shape[0]
+  p= sigmoid(points*line_parameters)
+  cross_entropy=-(1/n)*(np.log(p).T * y + np.log(1 - p).T * (1 - y))
+  return cross_entropy
+
+def grad_descent(line_parameters, points, y, alpha):
+    n=points.shape[0]
+    for i in range(500):
+        p = sigmoid(points*line_parameters)
+        gradient = (points.T * (p - y)) * (alpha / n)
+        line_parameters= line_parameters - gradient
+        w1 = line_parameters.item(0)
+        w2 = line_parameters.item(1)
+        b = line_parameters.item(2)
+        x1 = np.array([points[:,0].min(), points[:,0].max()])
+        x2 = -b/w2 + (x1*(-w1/w2))
+        draw(x1, x2)
+        print(calculate_error(line_parameters, points, y))
+
+n_pts=100
 np.random.seed(0)
-bias=np.ones(npts)
-topreg=np.array([np.random.normal(10, 2, npts), np.random.normal(12,2,npts), bias]).T
-botreg=np.array([np.random.normal(5,2, npts), np.random.normal(6,2,npts), bias]).T
-allpts=np.vstack((topreg, botreg))
+bias= np.ones(n_pts)
+top_region=np.array([np.random.normal(10,2,n_pts), np.random.normal(12,2,n_pts), bias]).T
+bottom_region= np.array([np.random.normal(5,2, n_pts), np.random.normal(6,2, n_pts), bias]).T
+all_points=np.vstack((top_region, bottom_region))
+line_parameters = np.matrix([np.zeros(3)]).T
+y=np.array([np.zeros(n_pts), np.ones(n_pts)]).reshape(n_pts*2, 1)
 
-w1= -0.2
-w2= -0.35
-b=3.5
-linepara=np.matrix([w1,w2,b]).T
-x1 = np.array([botreg[:, 0].min(), topreg[:, 1].max()])
-x2 = -b / w2 + x1 * (-w1 / w2)
-linearcombo=allpts*linepara
-prob=sigmoid(linearcombo)
-print(prob)
-
-_, ax = plt.subplots(figsize=(4,4))
-ax.scatter(botreg[:, 0], botreg[:, 1], color="b")
-ax.scatter(topreg[:, 0], topreg[:, 1], color="r")
-draw(x1,x2)
+_, ax= plt.subplots(figsize=(4,4))
+ax.scatter(top_region[:,0], top_region[:,1], color='r')
+ax.scatter(bottom_region[:,0], bottom_region[:,1], color='b')
+grad_descent(line_parameters, all_points, y, 0.06)
 plt.show()
